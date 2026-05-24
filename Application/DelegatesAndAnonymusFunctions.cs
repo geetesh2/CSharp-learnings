94618db61c4c4f9e6d5e72689d0b85afe6e2f938
Geetesh <geeteshpandey2@gmail.com>
2026-05-24 11:57:34 +0530

    Delegates and Anonymus functions

---
+++ Application/DelegatesAndAnonymusFunctions.cs
@@ -0,0 +1,25 @@
+﻿class DelegatesAndAnonymusFunctions
+{
+    delegate int Transfomer(int x);
+    static int Square(int x)
+    {
+        Console.WriteLine("Square called");
+        return x * x;
+    }
+    static int Cube(int x)
+    {
+        return x * x * x;
+    }
+    public static void Main(string[] args)
+    {
+        Transfomer transform = Square;
+        transform += Cube;
+
+        Transfomer transform2 = delegate (int x)
+        {
+           return x * x;
+        };
+
+        Console.WriteLine(transform(10)); // 100
+    }
+}
